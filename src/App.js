d33f0584dd1c24fc6dc5f9e93c933fc647a9975d
sunisakc <sunisa.k@gmail.com>
2019-03-29 11:36:37 +0100

    create component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos';
 
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Check emails',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Make Telephone calls',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Write articles',
+        completed: false
+      },
+      {
+        id: 4,
+        title: 'Ansers emails',
+        completed: false
+      }
+    ]
+  }
   render() {
+    
     return (
       <div className="App">
-        <h1>App</h1>
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
